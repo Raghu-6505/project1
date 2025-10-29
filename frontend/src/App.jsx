9096a97c6dd70951cfef38bb55f9dd8e43fbc5b6
dilshad <dilshadmdt@gmail.com>
2025-10-29 11:12:52 +0530

    title changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// Imports the Port component from the components folder.
+
 import Port from './components/Port.jsx'; 
 // Note: Changed the file extension to .jsx for better clarity if you are using that extension
 import './App.css';
